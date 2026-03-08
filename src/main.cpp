95f2af7622df7a5ebf13525faf0c4b9aeba54c78
Hakata <literallyme098@gmail.com>
2026-03-08 22:00:22 +0545

    sfml window render

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
-#include<stdio.h>
+#include<SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800,600),"Dungeon Escape");
+
+    //game loop 
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type== sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+}
+    
