348e083eb539546b76db3297d90f8ea96bd4933b
Hakata <literallyme098@gmail.com>
2026-03-10 21:01:17 +0545

    rendering a rectangel and movig it using keyboard

---
+++ build/CMakeFiles/DungeonEscape.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DungeonEscape.dir/src/main.cpp.o"
+  "CMakeFiles/DungeonEscape.dir/src/main.cpp.o.d"
+  "DungeonEscape"
+  "DungeonEscape.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DungeonEscape.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,18 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800,600),"Dungeon Escape");
 
+    float speed = 100;
+    sf::RectangleShape player;
+    player.setSize(sf::Vector2f(50.f,50.f));
+    player.setFillColor(sf::Color::White);
+    player.setPosition(400,300);
+
     //game loop 
+    sf::Clock clock;
     while(window.isOpen())
     {
+
+        float dt = clock.restart().asSeconds();
         sf::Event event;
         while(window.pollEvent(event))
         {
@@ -15,7 +24,25 @@ int main()
                 window.close();
             }
         }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            player.move(0,-speed*dt);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            player.move(0,+speed*dt);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            player.move(-speed*dt,0);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            player.move(speed*dt,0);
+        }
+
         window.clear(sf::Color::Black);
+        window.draw(player);
         window.display();
     }
 }
