6ac461174c63a83509f1f614d51fca8dff93715b
Hakata <literallyme098@gmail.com>
2026-03-14 21:08:54 +0545

    added chaser and stalker - derived from enemy class

---
+++ include/utils.hpp
@@ -0,0 +1,10 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+class utility
+{
+    public:
+    static int randomNumberGenerator(int min , int max);
+};
+
+#endif
---
+++ src/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.hpp"
+#include<random>
+int utility::randomNumberGenerator(int min , int max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(min,max);
+
+    return dist(gen);
+}
+
+float utility::randomFloatGenerator(float min,float max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(min,max);
+}
