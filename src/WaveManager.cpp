dd5bc01b8f4c1cfd92331ae777896110bd3b4f4f
Hakata <literallyme098@gmail.com>
2026-03-25 21:20:40 +0545

    wave manager finshed next wave generator done

--- src/WaveManager.cpp
+++ src/WaveManager.cpp
@@ -1,4 +1,7 @@
 #include "WaveManager.hpp"
+#include "utils.hpp"
+#include "Chaser.hpp"
+#include "Stalker.hpp"
 
 WaveManager::WaveManager(std::vector<Enemy*>& enemiesRef):enemies(enemiesRef)
 {
@@ -8,7 +11,31 @@ WaveManager::WaveManager(std::vector<Enemy*>& enemiesRef):enemies(enemiesRef)
 
 
 bool WaveManager::isWaveCleared()
-{
-    
-
+{ 
+    for(Enemy* e: enemies)
+    {
+        if(e->getAliveCondition()) return false;
+    }
+    return true;    
 }
+
+void WaveManager::spawnNextWave()
+{
+    for(Enemy* e:enemies)
+    {
+        delete e;
+    }
+    enemies.clear();//clear the vector to clear the dangling pointers
+    currentWave++;
+    for(int i =1 ;i<(2+currentWave);i++)
+    {
+        if(i%2!=0) 
+        {
+            enemies.push_back(new Chaser(30+2*currentWave,10+2*currentWave,100.f+2*currentWave,sf::Vector2f(utility::randomFloatGenerator(0,790),utility::randomFloatGenerator(0,590))));
+        }
+        else
+        {
+            enemies.push_back(new Stalker(100+2*currentWave,25+2*currentWave,59.f+2*currentWave,sf::Vector2f(utility::randomFloatGenerator(0,790),utility::randomFloatGenerator(0,590))));
+        }
+    }
+}   
